c375f71205b24b19eaddcda309cd429f27da4bf2
Mayron Urieles <maironurielesan@gmail.com>
2021-11-08 22:24:04 -0500

    modificando acceso de rutas

--- models/Conexion.php
+++ models/Conexion.php
@@ -3,7 +3,7 @@
     {   
         public static function conectar(){
             try {
-                include "/Applications/MAMP/htdocs/prueba_compamny/config/database.php";
+                include "https://globalsystemcompany.herokuapp.com/config/database.php";
                 //$conexion = mysqli_connect($config["host"], $config["user"], $config["pass"], $config["database"]);
 
                 $conexion = new PDO($config["driver"].":host=".$config["host"].";dbname=".$config["database"],$config["user"], $config["pass"]);
